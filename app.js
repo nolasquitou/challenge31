3e6e4c53fe91cc21f2c9ea0337863ff450a1ad8d
Nolasquito <nolascopalavecino.com>
2023-05-20 18:09:52 -0300

    challenge 31 camb

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-// function Peliculas (titulo, genero, año, sinopsis){
-//     this.titulo = titulo;
-//     this.genero = genero;
-//     this.año = año;
-//     this.sinopsis = sinopsis;
-// }
-
-// let anabel = new Peliculas("Anabel", "Terror", 1990, "Muñeca diavolica");
-// let chuky = new Peliculas("Chuky", ["Terror", "Suspenso"], 1987, "muñeco diavolico");
-// let spiderman = new Peliculas("Spider-Man", "Accion", 1997, "El asombroso hombre araña");
-
-
-// const verPelicula = () => {
-//     console.log(anabel)
-// }
-
 class Peliculas {
     constructor(titulo, genero, año, sinopsis) {
         this.titulo = titulo;
